60899b6301466bf87c64d9ad1e8f64486158f49b
duha demirpolat <12binli@protonmail.com>
2023-09-09 14:03:58 +0300

    ilk versiyon

---
+++ kdv_programi/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //değişkenleri oluştur.
+
+        float fiyat, kdvOrani, kdv ;
+
+        //Scanner sınıfını tanımla.
+
+        Scanner input = new Scanner(System.in);
+
+        //kullanıcıdan değeri al.
+
+        System.out.println("ürünün fiyatını giriniz: ");
+
+        fiyat = input.nextFloat();
+
+        //kdv'yi hesapla
+
+        kdvOrani = fiyat<=1000 ? 0.18f : 0.08f;
+
+        kdv = fiyat * kdvOrani;
+
+        //sonuçları ekrana yazdır
+
+        System.out.println("KDV'siz fiyat: " + fiyat);
+
+        System.out.println("KDV tutarı: " + kdv);
+
+        System.out.println("KDV'li fiyat: " + (kdv + fiyat));
+
+
+
+    }
+}
