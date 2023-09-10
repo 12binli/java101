0c2d6784af05188e609ec3a9cd2b05406c00ed74
duha demirpolat <12binli@protonmail.com>
2023-09-10 15:04:46 +0300

    eksik kod

---
+++ java_datatypes/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //değişkenleri oluştur.
+
+        int T;
+
+        //Scanner sınıfını tanımla.
+
+        Scanner input = new Scanner(System.in);
+
+        //kullanıcıdan değerleri al.
+
+        System.out.println("Kaç tane sorgulama yapılacak? : ");
+
+        T = input.nextInt();
+
+        for (int i=0; i<T;i++) {
+
+
+
+        }
+
+    }
+
+}
