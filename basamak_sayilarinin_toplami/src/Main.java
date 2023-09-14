412790a905528bfb309b288609ed6ac4364392ae
duha demirpolat <12binli@protonmail.com>
2023-09-14 15:22:56 +0300

    ilk versiyon

---
+++ basamak_sayilarinin_toplami/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //değişkenleri oluştur.
+
+        int n,b = 0,bd, sonuc=0;
+
+        //scanner sınıfını tanımla.
+
+        Scanner input = new Scanner(System.in);
+
+        //kullanıcıdan değeri al.
+
+        System.out.print("sayı giriniz : ");
+
+        n = input.nextInt();
+
+        int gn = n;
+
+        //sonucu ekrana yazdır.
+
+        while (gn != 0) {
+
+            gn /=10;
+
+            b++;
+
+        }
+
+        gn = n;
+
+        while (gn !=0) {
+
+            bd = gn % 10;
+
+            sonuc+=bd;
+
+            gn/=10;
+
+        }
+
+        System.out.print(n+" sayısının basamak sayılarının toplamı : "+sonuc);
+
+    }
+
+}
