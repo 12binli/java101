0800b82e5f78554c3ad736216c77b48876033343
duha demirpolat <12binli@protonmail.com>
2023-09-16 17:29:31 +0300

    ekleme

--- java_datatypes/src/Main.java
+++ java_datatypes/src/Main.java
@@ -4,21 +4,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //değişkenleri oluştur.
-
-        int T;
-
-        //Scanner sınıfını tanımla.
+        int t;
+        long n;
 
         Scanner input = new Scanner(System.in);
 
-        //kullanıcıdan değerleri al.
+        t = input.nextInt();
 
-        System.out.println("Kaç tane sorgulama yapılacak? : ");
+        for (int i=0; i<t; i++) {
 
-        T = input.nextInt();
+            n = input.nextLong();
 
-        for (int i=0; i<T;i++) {
+            System.out.println(n+" can be fitted in:");
+
+            if ( (n>=-128) && (n<=127) ) {
+
+                System.out.println("* byte");
+
+            }
+
+            if ( (n>=-32768) && (n<=32767) ) {
+
+                System.out.println("* short");
+
+            }
+
+            if ( (n>=-2147483648) && (n<=2147483647) ) {
+
+                System.out.println("* int");
+
+            }
+
+            if ( (n>=-9223372036854775808L) && (n<=9223372036854775807L) ) {
+
+                System.out.println("* long");
+
+            }
 
 
 
