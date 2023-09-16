0bfb0a7a954fd38996b0d4806f2b70d8f5fa474a
duha demirpolat <12binli@protonmail.com>
2023-09-16 11:13:23 +0300

    ilk versiyon

---
+++ ters_ucgen/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //değişkenleri oluştur.
+
+        int n;
+
+        //scanner sınfını tanımla.
+
+        Scanner input = new Scanner(System.in);
+
+        //kullanıcıdan değeri al.
+
+        System.out.print("bir sayı giriniz : ");
+
+        n = input.nextInt();
+
+        //sonucu ekrana yazdır.
+
+        for (int i = 1; i <= n; i++ ) {
+
+            for (int j = 1; j < i; j++ ) {
+
+                System.out.print(" ");
+
+            }
+
+            for (int k = 0 ; k <= 2*(n-i); k++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println("");
+
+        }
+
+
+    }
+
+}
